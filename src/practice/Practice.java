07a77f1cb1df7920273fd1bb013d5843835c93f1
hosokawayuika <hosokawayuika@192.168.3.11>
2024-05-11 17:44:01 +0900

    占い

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -2,7 +2,18 @@ package practice;
 
 public class Practice {
 	public static void main(String[] args) {
-		int r = new java.util.Random().nextInt(90);
-		System.out.println("あなたは多分" + r + "才ですよね");
+		System.out.println("ようこそ、占いの館へ");
+		System.out.println("あなたの名前を入力してください");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("あなたの年齢を入力してください");
+		String ageString = new java.util.Scanner(System.in).nextLine();
+		int age = Integer.parseInt(ageString);
+		int fortune = new java.util.Random().nextInt(4);
+		fortune++;
+		System.out.println("占いの結果が出ました！");
+		System.out.println
+		(age + "歳の" + name + "さん。あなたの運気番号は" + fortune + "です");
+		System.out.println("(1:大吉 2:中吉 3:吉 4:凶)");
+		
 	}
 }
