69e00242513d73c8c51a1db72811424d867bc9d6
hosokawa3 <clover091039@gmail.com>
2024-05-30 14:37:21 +0900

    数並べ

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -2,18 +2,13 @@ package practice;
 
 public class Practice {
 	public static void main(String[] args) {
-		System.out.println("数あてゲーム");
-		int ans = new java.util.Random().nextInt(10);
-		for (int i = 0; i < 5; ++i) {
-			System.out.println("0~9の数字を入力してください");
-			int num = new java.util.Scanner(System.in).nextInt();
-			if (num == ans) {
-				System.out.println("アタリ！");
-				break;
+		for (int i = 1; i <= 10; i++) {
+			if (i < 10) {
+				System.out.print(i + ",");
 			} else {
-				System.out.println("違います");
+				System.out.print(i);
 			}
+			
 		}
-		System.out.println("ゲームを終了します");
 	}
 }
