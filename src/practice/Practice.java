1d669fb7f10b5733a53bbb539b8dc430ceb2dd4d
hosokawa3 <clover091039@gmail.com>
2024-06-02 11:20:33 +0900

    メソッド練習

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -1,14 +1,14 @@
 package practice;
 
 public class Practice {
+	public static void methodA() {
+		System.out.println("methodA");
+		methodB();
+	}
+	public static void methodB() {
+		System.out.println("methodB");
+	}
 	public static void main(String[] args) {
-		for (int i = 1; i <= 10; i++) {
-			if (i < 10) {
-				System.out.print(i + ",");
-			} else {
-				System.out.print(i);
-			}
-			
-		}
+		methodA();
 	}
 }
