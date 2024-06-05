ccf86564c2d1e4fcdf655e6b669693d41833ec82
hosokawa3 <clover091039@gmail.com>
2024-06-05 16:10:53 +0900

    オーバーロード

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -6,7 +6,14 @@ public class Practice {
 		System.out.println("件名:" + title);
 		System.out.println("本文:" + text);
 	}
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名:無題");
+		System.out.println("本文:" + text);
+	}
+	
 	public static void main(String[] args) {
-		email("メールのタイトル", "メールの宛先アドレス", "メールの本文");
+//		email("メールのタイトル", "メールの宛先アドレス", "メールの本文");
+		email("メールの宛先アドレス", "メールの本文");
 	}
 }
