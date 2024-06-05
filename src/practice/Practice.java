96012ee258068a4097c4090fead3c9fd86a72786
hosokawa3 <clover091039@gmail.com>
2024-06-05 11:36:27 +0900

    自己紹介文メソッド

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -1,13 +1,15 @@
 package practice;
 
 public class Practice {
-	public static void printArray(int[] array) {
-		for (int element : array) {
-			System.out.println(element);
-		}
+	public static void introduceOneself() {
+		String name = "湊祐輔";
+		int age = 22;
+		double tall = 169.9;
+		char animal = '辰';
+		System.out.println
+		("私の名前は" + name + "です。歳は" + age + "歳です。身長は" + tall + "cmです。十二支は" + animal + "です。");
 	}
 	public static void main(String[] args) {
-		int[] array = {1, 2, 3};
-		printArray(array);
+		introduceOneself();
 	}
 }
