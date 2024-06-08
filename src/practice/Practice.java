e061d5c6d3a20d97fa9f206db688f6a903ce91a1
hosokawa3 <clover091039@gmail.com>
2024-06-08 12:56:08 +0900

    計算プログラム

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -1,15 +1,11 @@
 package practice;
 
 public class Practice {
-	public static double calcTriangleArea(double bottom, double height) {
-		return (bottom * height) / 2;
-	}
-	public static double calcCircleArea(double radius) {
-		return (radius * radius) * 3.14;
-	}
-	
 	public static void main(String[] args) {
-		System.out.println("三角形の面積は" + calcTriangleArea(10.0, 5.0) + "cm2");
-		System.out.println("円の面積は" + calcCircleArea(5.0) + "cm2");
+		int a = 10;
+		int b = 2;
+		int total = CalcLogic.tasu(a, b);
+		int delta = CalcLogic.hiku(a, b);
+		System.out.println("足すと" + total + "引くと" + delta);
 	}
 }
